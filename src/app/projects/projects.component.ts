bfe9b5980f0b009685aba79e9426e32fce30f323
itzik savaia <44684932+itzikking@users.noreply.github.com>
2020-09-07 22:27:39 +0300

    as

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,13 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
   projects = [
-    // { name: "Network-Scanner", description: "The project chack if ping is ok or not.", href: "https://networkscanner.herokuapp.com/" },
     {
       name: "M-Market",
       description: "Super market online",
       href: "https://m-market-client.herokuapp.com/",
       imgFront: "../../assets/projects/m-market/user.bmp",
       imgBack: "../../assets/projects/m-market/admin.bmp"
+    },
+    {
+      name: "NETMOVIES",
+      description: "terilers",
+      href: "https://net-movies-client.herokuapp.com/",
+      imgFront: "../../assets/projects/net-movies/netmovies-512x512.png",
+      imgBack: "../../assets/projects/net-movies/movies.bmp"
     }
   ]
 

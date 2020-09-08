321242c4b4b8500e44ecf55afa875e6864747f74
itzik savaia <44684932+itzikking@users.noreply.github.com>
2020-09-08 19:07:54 +0300

    as

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -16,7 +16,7 @@ export class ProjectsComponent implements OnInit {
     },
     {
       name: "NETMOVIES",
-      description: "movies trailer",
+      description: "Movies trailer",
       href: "https://net-movies-client.herokuapp.com/",
       imgFront: "../../assets/projects/net-movies/netmovies-512x512.png",
       imgBack: "../../assets/projects/net-movies/movies.bmp"

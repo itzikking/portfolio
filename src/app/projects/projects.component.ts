ba7690660e17fea24ecbb25b18a5e7bf0d87e502
itzik savaia <44684932+itzikking@users.noreply.github.com>
2020-09-12 21:12:27 +0300

    as

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -11,22 +11,22 @@ export class ProjectsComponent implements OnInit {
       name: "M-Market",
       description: "Super market online",
       href: "https://m-market-client.herokuapp.com/",
-      imgFront: "../../assets/projects/m-market/user.bmp",
-      imgBack: "../../assets/projects/m-market/admin.bmp"
+      imgFront: "../../assets/projects/m-market/user.png",
+      imgBack: "../../assets/projects/m-market/admin.png"
     },
     {
       name: "NETMOVIES",
       description: "Movies trailer",
       href: "https://net-movies-client.herokuapp.com/",
       imgFront: "../../assets/projects/net-movies/netmovies-512x512.png",
-      imgBack: "../../assets/projects/net-movies/movies.bmp"
+      imgBack: "../../assets/projects/net-movies/movies.png"
     },
     {
       name: "Mosesbox-fit",
       description: "MMA",
       href: "https://mosesbox-fit.herokuapp.com/",
-      imgFront: "../../assets/projects/Mosesbox-fit/moses.bmp",
-      imgBack: "../../assets/projects/Mosesbox-fit/moses.bmp"
+      imgFront: "../../assets/projects/Mosesbox-fit/moses.png",
+      imgBack: "../../assets/projects/Mosesbox-fit/moses.png"
     }
   ]
 
